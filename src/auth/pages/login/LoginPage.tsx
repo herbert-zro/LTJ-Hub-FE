c6d6f9d16a231889a966d14306e39517cb5e6a12
Herbert Gutierrez <herbeherbert.gutierrez@zr0-i.com>
2026-02-19 15:20:32 -0600

    LoginPage Responsive included

--- src/auth/pages/login/LoginPage.tsx
+++ src/auth/pages/login/LoginPage.tsx
@@ -1,15 +1,11 @@
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export const LoginPage = ({
-  className,
-  ...props
-}: React.ComponentProps<"div">) => {
+export const LoginPage = () => {
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
+    <div className="flex flex-col gap-6">
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
           <form className="p-6 md:p-8">
@@ -32,18 +28,27 @@ export const LoginPage = ({
               <div className="grid gap-2">
                 <div className="flex items-center">
                   <Label htmlFor="password">Contraseña</Label>
-                  <a
-                    href="#"
-                    className="ml-auto text-sm underline-offset-2 hover:underline"
-                  >
-                    Olvido contraseña?
-                  </a>
                 </div>
-                <Input id="password" type="password" required />
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="Contraseña"
+                  required
+                />
               </div>
               <Button type="submit" className="w-full">
                 Iniciar sesión
               </Button>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <a
+                    href="#"
+                    className="text-sm underline-offset-2 hover:underline"
+                  >
+                    Olvido contraseña?
+                  </a>
+                </div>
+              </div>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                 <span className="relative z-10 bg-background px-2 text-muted-foreground">
                   Bienvenido de nuevo
@@ -51,7 +56,7 @@ export const LoginPage = ({
               </div>
             </div>
           </form>
-          <div className="relative hidden bg-muted md:block">
+          <div className="relative order-first h-40 bg-muted md:order-0 md:h-auto">
             <img
               src="/placeholder.svg"
               alt="Image"
